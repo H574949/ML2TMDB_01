c34cc66c8ae203d9e0d1a769479c62d74835a13c
H574949 <54100056+H574949@users.noreply.github.com>
2021-11-12 22:12:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class StreamlitApp:
     
     def __init__(self):
         ppath = Path(__file__).parents[1] / 'ml2tmdb_01/revenue_pred'
-        self.model = load_model(ppath) 
+        self.model = load_model('revenue_pred') 
         self.save_fn = 'path.csv'
 
     def predict(self, input_data):
@@ -28,7 +28,7 @@ class StreamlitApp:
     
     def run(self):
         ipath = Path(__file__).parents[1] / 'ml2tmdb_01/image.png'
-        image = Image.open(ipath)
+        image = Image.open('image.png')
         st.image(image, use_column_width=False)
 
         
@@ -42,20 +42,21 @@ class StreamlitApp:
             runtime = st.number_input('Runtime', min_value=1, max_value=600, value=1)
             genre = st.selectbox('Select genres:', ['Action', 'Comedy', 'Romance', 'Sci-fi', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Adventure', 'Documentary'])
             BTC = 0
-            production_companies = st.text_input("Production company")
+            production_companies = st.selectbox('Select Production companies:', ['Disney', 'Warner Bros', 'Universal' , 'Sony', 'Paramount', 'Miramax', 'Columbia', 'Tristar' ,'20th Century'])
             original_language = st.selectbox('Original Language', ['en', 'hi','ko','sr','fr','it','nl','zh','es','cs','ta','cn','ru','tr','ja','fa','sv','de','te','pt','mr'])
             date = st.date_input("Release date")
             if st.checkbox('Belongs to collection'):
                 BTC = 1
-
-     
-
+            
+            
+            print(genre)
             output =''
-            input_dict = {'budget':budget, 'popularity':popularity, 'runtime':runtime, 'genres':genre, 'production_companies':production_companies,
-                        'original_language':original_language, 'release_date':date, 'belongs_to_collection':BTC}
-            input_df = pd.DataFrame(input_dict, index=[0])
+            
 
             if st.button('Predict'):
+                input_dict = {'budget':budget, 'popularity':popularity, 'runtime':runtime, 'genres':genre, 'production_companies':production_companies,
+                        'original_language':original_language, 'release_date':date, 'belongs_to_collection':BTC}
+                input_df = pd.DataFrame(input_dict, index=[0])
                 output = self.predict(input_df)
                 self.store_prediction(output)
                 output = (output['Label'][0])/1000000  
