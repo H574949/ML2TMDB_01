9196c63841e1eb574ff6d218f9a00ad08c2ba232
H574949 <54100056+H574949@users.noreply.github.com>
2021-11-12 18:48:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 class StreamlitApp:
     
     def __init__(self):
-        ppath = Path(__file__).parents[1] / '/ml2tmdb_02/revenue_pred'
+        ppath = Path(__file__).parents[1] / 'ml2tmdb_02/revenue_pred'
         self.model = load_model(ppath) # app/revenue_pred.pkl
         self.save_fn = 'path.csv'
 
@@ -27,7 +27,7 @@ class StreamlitApp:
             output_df.to_csv(self.save_fn, index=False)
     
     def run(self):
-        ipath = Path(__file__).parents[0] / 'ml2tmdb_01/image.png'
+        ipath = Path(__file__).parents[1] / 'ml2tmdb_01/image.png'
         image = Image.open(ipath)
         st.image(image, use_column_width=False)
 
